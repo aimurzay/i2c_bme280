89196c7b9f0689ab40c1a41f3c3325552df3d155
Dimash Aimurzayev <aimurzay@gmail.com>
2019-09-02 11:16:22 +0600

    i2c interface with bme280 sensor

---
+++ MDK-ARM/BME280_I2C/startup_stm32l031xx.d
@@ -0,0 +1,1 @@
+bme280_i2c\startup_stm32l031xx.o: startup_stm32l031xx.s
---
+++ MDK-ARM/RTE/_BME280_I2C/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'BME280_I2C' 
+ * Target:  'BME280_I2C' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
